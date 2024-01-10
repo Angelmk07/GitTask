a47fc907ddee7c6e3be81c03dab528f5c685943a
Angelmk07 <mkrupin07@gmail.com>
2024-01-10 18:49:10 +0300

    Code

--- ConsoleApp7/GitTask/Program.cs
+++ ConsoleApp7/GitTask/Program.cs
@@ -1,1 +1,40 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Random rnd = new Random();
+Console.WriteLine("Как вас зовут");
+string name = Console.ReadLine();
+Console.WriteLine($"Здраствуйте, {name} попробуйте угадать число ");
+bool rematch = true;
+int tryes = 0;
+string answers;
+int unnow_number = rnd.Next(1, 101);
+while (rematch)
+{
+
+    int answer = int.Parse(Console.ReadLine());
+    if (answer == unnow_number)
+    {
+        Console.WriteLine("Вы угадали ");
+        Console.WriteLine($"Вам понадобилось,{tryes} попыток. Хотите еще раз?");
+        answers = Console.ReadLine();
+        if (answers == "Да")
+        {
+            unnow_number = rnd.Next(1, 101);
+            Console.WriteLine("Попробуйте угадать число");
+            continue;
+        }
+        else
+        {
+            rematch = false;
+        }
+
+    }
+    else if (answer > unnow_number)
+    {
+        Console.WriteLine("много");
+        tryes++;
+    }
+    else if (answer < unnow_number)
+    {
+        Console.WriteLine("мало");
+        tryes++;
+    }
+}
